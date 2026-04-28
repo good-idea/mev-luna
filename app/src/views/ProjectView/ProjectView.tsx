8a76c22447158f3bc762f9a8db769cc77128e44f
Joseph <joseph@good-idea.studio>
2026-04-27 17:20:30 -0700

    fix(app): move project year, materials, notes to description zone

--- app/src/views/ProjectView/ProjectView.tsx
+++ app/src/views/ProjectView/ProjectView.tsx
@@ -21,6 +21,7 @@ export const ProjectView: React.FC<ProjectViewProps> = ({ project }) => {
     gallery,
     year,
   } = project;
+  const research = definitely(relatedResearch);
   return (
     <>
       <ProjectColumns>
@@ -28,21 +29,23 @@ export const ProjectView: React.FC<ProjectViewProps> = ({ project }) => {
           <x.h2 mt={0}>{title}</x.h2>
           <ProjectDescription>
             <RichText text={description} />
+            <x.div mt={8}>
+              <x.p my={0}>{year}</x.p>
+              <RichText text={materials} />
+            </x.div>
+            <x.div mt={4}>
+              <RichText text={notes} />
+            </x.div>
           </ProjectDescription>
         </div>
         {gallery ? <Gallery gallery={gallery} /> : <div />}
       </ProjectColumns>
-      <ProjectColumns>
-        <div>
-          <RichText text={notes} />
-        </div>
-        <div>
-          <x.p my={0}>{year}</x.p>
-          <RichText text={materials} />
-        </div>
-      </ProjectColumns>
-      <x.hr border="none" borderBottom="1px solid black" m={0} />
-      <ResearchList research={definitely(relatedResearch)} />
+      {research.length ? (
+        <>
+          <x.hr border="none" borderBottom="1px solid black" m={0} />
+          <ResearchList research={definitely(relatedResearch)} />
+        </>
+      ) : null}
     </>
   );
 };
